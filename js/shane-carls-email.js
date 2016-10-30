fdeebef92aa8b7fefd371c904d809358887f9ca6
Shane <shane@shanerice.me>
2016-10-30 13:15:36 -0700

    Adding to GitLab

---
+++ js/shane-carls-email.js
@@ -0,0 +1,7 @@
+/*<![CDATA[*/
+var emailriddlerarray=[115,104,97,110,101,64,99,97,114,108,115,46,115,105,116,101]
+var encryptedemail_id26='' //variable to contain encrypted email
+for (var i=0; i<emailriddlerarray.length; i++)
+ encryptedemail_id26+=String.fromCharCode(emailriddlerarray[i])
+document.write('<a href="mailto:'+encryptedemail_id26+'">'+encryptedemail_id26+'</a>')
+/*]]>*/
