11eeb38f2becce225dfca7d18fd1c1623c0337e9
Shane <shane@shanerice.me>
2016-12-18 00:58:14 -0800

    converted to django

---
+++ home/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from django.views.generic.base import RedirectView
+from django.contrib import admin
+from carls_site.settings import STATIC_URL, config
+
+import home.views as home
+
+urlpatterns = [
+    url(r'^$', home.home, name='home'),
+    url(r'^favicon\.ico$', RedirectView.as_view(url=STATIC_URL + 'favicon.ico')),
+    url(r'^github/', home.github, name='github'),
+    url(r'^add/', RedirectView.as_view(url=config.get('Discord', 'add_url')), name='add'),
+    url(r'^discord/', RedirectView.as_view(url=config.get('Discord', 'invite_url')), name='discord'),
+    url(r'^admin/', admin.site.urls, name="django_admin"),
+]
